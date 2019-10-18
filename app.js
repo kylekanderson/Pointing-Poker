28b91a0f5862611de9ee71f205ca2325f131668e
Kyle Anderson <kylekandderson@gmail.com>
2019-10-17 20:50:56 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,106 @@
+// various modules required by the express framework
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+//create an empty list of users
+var users = {};
+
+//create an empty list of votes
+var votes = {};
+
+/*
+================================================================================================================
+Socket Event Listeners
+================================================================================================================
+*/
+io.on('connection', socket => {
+
+    //when a new user connects...
+    socket.on('new-user', name => {
+        //add the name emitted from the client to the users list
+        users[socket.id] = name;
+        io.emit('user-connected', users);
+    });
+
+    //when a user votes...
+    socket.on('send-vote', vote => {
+        //add the vote emitted from the client to the votes list
+        votes[socket.id] = vote;
+        io.emit('vote', {vote: vote, user: users[socket.id]});
+    });
+
+    //when voting is finished, emit back the full votes list
+    socket.on('voting-finished', () => {
+        io.emit('voting-results', {votes: votes});
+    })
+
+    socket.on('disconnect', () => {
+        //using broadcast here because the user that's disconnecting
+        //doesn't need to receive this response from the server
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        //remove the disconnecting user from the user list
+        delete users[socket.id]
+    })
+
+    socket.on('reset', () => {
+        //when the reset signal is sent, empty the votes list
+        votes = {};
+        io.emit('reset-page');
+    })
+});
+
+
+
+/*
+================================================================================================================
+View Engine Setup (stock express code)
+================================================================================================================
+*/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  res.io = io;
+  next();
+});
+
+app.use('/', indexRouter);
+
+app.use('/favicon.ico', express.static('public/images/favicon.ico'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+/*
+================================================================================================================
+Error Handler (stock express code)
+================================================================================================================
+*/
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = {app: app, server: server};
---
+++ public/javascripts/script.js
@@ -0,0 +1,232 @@
+const port = 80;
+const socket = io("http://10.0.0.99:" + port); // initialize our socket.io variable
+const cardForm = document.getElementById("card-container"); // card container element
+const resetContainer = document.getElementById("reset-container"); // reset button container
+const name = prompt("What is your name?"); // get the users name
+const votingContainer = document.getElementById("voting-container"); // get the voting container element
+const messageContainer = document.getElementById("message-container"); // roster container
+
+var votingFinished = false; // used to track if each vote is the last vote or not
+
+socket.emit("new-user", name); // emit a new user connection to the socket
+
+/*
+================================================================================================================
+Page Event Listeners
+================================================================================================================
+*/
+cardForm.addEventListener("submit", e => {
+    e.preventDefault(); // prevent the page from reloading
+    const vote = document.activeElement.textContent; // get the vote value from the text of the button
+    consoleLog("vote cast:" + vote);
+    socket.emit("send-vote", vote); // emit the vote to the socket
+});
+
+resetContainer.addEventListener("reset", e => {
+    e.preventDefault(); // prevent the page from reloading
+    socket.emit("reset"); // emit the reset command to the socket
+});
+
+/*
+================================================================================================================
+Socket Event Listeners
+================================================================================================================
+*/
+socket.on("user-connected", users => {
+    if (firstLoad()) {
+        fillVotingContainer(users);
+    } else {
+        newVoter = Object.values(users)[Object.values(users).length - 1];
+        addVoter(`${newVoter}`);
+    }
+});
+
+socket.on("vote", data => {
+    castVote(`${data.user}`, `${data.vote}`);
+});
+
+socket.on("voting-results", votes => {
+    consoleLog(votingFinished);
+    if (!votingFinished) {
+        updateCards(votes);
+        votingFinished = true;
+    }
+});
+
+socket.on("user-disconnected", user => {
+    consoleLog("user disconnected");
+    removeVoter(user);
+});
+
+socket.on("reset-page", () => {
+    reset();
+    reset();
+});
+
+/*
+================================================================================================================
+Functions
+================================================================================================================
+*/
+// if the voting container only contains 1 element, then there's only 1 user connected
+function firstLoad() {
+    consoleLog(votingContainer.childElementCount);
+    if (votingContainer.childElementCount == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// iterate over the users object and call the addVoter function to add each user to the voting container
+function fillVotingContainer(users) {
+    for (const key in users) {
+        if (users.hasOwnProperty(key)) {
+            const userName = users[key];
+            consoleLog("iterating through users... current user: " + userName);
+            addVoter(userName);
+        }
+    }
+}
+
+// when a user votes, find their poker card (by matching the cards ID to the users name)
+// and then update the card from a blank card to a card with a pattern
+// finally, check if the current vote was the last vote or not; if so, emit 'voting-finished' to the socket
+function castVote(user, vote) {
+    var pokerCard = document.getElementById(user);
+    pokerCard.setAttribute("src", "/images/poker_card.png");
+    consoleLog("pokerCard: " + pokerCard);
+    if (voteFinished()) {
+        consoleLog("voting finished");
+        socket.emit("voting-finished");
+    }
+}
+
+// takes a user (string) parameter and adds:
+// 1) a voting card to the voting container for the user
+// 2) the users name to the roster container
+function addVoter(user) {
+    consoleLog("calling addVoter with user: " + user);
+    var pokerCard = document.createElement("img");
+    pokerCard.setAttribute("src", "/images/blank_card.png"); // blank card because the user hasn't voted yet
+    pokerCard.setAttribute("class", "pokerCard");
+    pokerCard.setAttribute("id", user); // set the ID of the card to be equal to the user's name
+    pokerCard.setAttribute("alt", "pointing poker card");
+    votingContainer.appendChild(pokerCard);
+    var messageSpace = document.createElement("p");
+    messageSpace.setAttribute("class", "joinMessage");
+    messageSpace.setAttribute("id", user); // add the users name to the roster
+    messageContainer.appendChild(messageSpace);
+    var resultText = document.createTextNode(user);
+    messageSpace.appendChild(resultText);
+}
+
+// takses a user(string) parameter and removes:
+// 1) their card from the voting container
+// 2) their name from the roster
+function removeVoter(user) {
+    var pokerCard = document.getElementById(user);
+    votingContainer.removeChild(pokerCard);
+    var rosterEntry = document.getElementById(user);
+    messageContainer.removeChild(rosterEntry);
+}
+
+// checks the 'src' attribute of all cards in the voting container
+// if any of them are still using the blank_card image, returns false (else, returns true)
+function voteFinished() {
+    var finished = true;
+    var arrCards = votingContainer.getElementsByTagName("IMG");
+    for (const card in arrCards) {
+        if (arrCards.hasOwnProperty(card)) {
+            const element = arrCards[card];
+            if (element.getAttribute("src") == "/images/blank_card.png") {
+                finished = false;
+            }
+        }
+    }
+    return finished;
+}
+
+// resets:
+// 1) all cards to the blank_card image
+// 2) all results to empty
+// 3) votingFinished variable to false
+function reset() {
+    var arrCards = votingContainer.getElementsByTagName("IMG");
+    for (const card in arrCards) {
+        if (arrCards.hasOwnProperty(card)) {
+            const element = arrCards[card];
+            element.setAttribute("src", "/images/blank_card.png");
+        }
+    }
+    var arrResults = votingContainer.getElementsByTagName("P");
+    for (const result in arrResults) {
+        if (arrResults.hasOwnProperty(result)) {
+            const element = arrResults[result];
+            element.parentNode.removeChild(element);
+        }
+    }
+    votingFinished = false;
+    consoleLog(votingContainer.children);
+}
+
+// takes a votes object and:
+// 1) decorates the voting container with a results header
+// 2) tallies up the votes
+// 3) calls some helper methods to draw the votes and stats to the page
+function updateCards(votes) {
+    var voteSum = 0;
+    var voteCount = 0;
+    consoleLog("calling updateCards");
+    var resultHeader = document.createElement("p");
+    resultHeader.setAttribute("class", "resultHeader");
+    votingContainer.appendChild(resultHeader);
+    var resultHeaderText = document.createTextNode("Results:");
+    resultHeader.appendChild(resultHeaderText);
+    for (const user in votes) {
+        if (votes.hasOwnProperty(user)) {
+            const singleVote = votes[user];
+            for (const vote in singleVote) {
+                if (singleVote.hasOwnProperty(vote)) {
+                    var result = parseInt(singleVote[vote]);
+                    var resultSpace = document.createElement("p");
+                    voteCount = voteCount + 1;
+                    consoleLog(result);
+                    voteSum = voteSum + result;
+                    drawVote(resultSpace, result, voteCount);
+                }
+            }
+        }
+    }
+    consoleLog(voteSum);
+    consoleLog(voteCount);
+    var breakPoint = document.createElement("br");
+    resultSpace.appendChild(breakPoint);
+    var voteAverage = voteSum / voteCount;
+    consoleLog("vote average: " + voteAverage);
+    drawStats(voteAverage);
+}
+
+// draws a result (int) and voteCount (int) to the page
+function drawVote(resultSpace, result, voteCount) {
+    resultSpace.setAttribute("class", "votingResult");
+    votingContainer.appendChild(resultSpace);
+    var resultText = document.createTextNode("Vote " + voteCount + ": " + result);
+    resultSpace.appendChild(resultText);
+}
+
+// draws a voteAverage (int) to the page
+function drawStats(voteAverage) {
+    var averageSpace = document.createElement("p");
+    averageSpace.setAttribute("class", "averageResult");
+    votingContainer.appendChild(averageSpace);
+    var averageText = document.createTextNode("Average: " + voteAverage);
+    averageSpace.appendChild(averageText);
+}
+
+// using this to only enable logging when the app is running in DEV (port 3000)
+function consoleLog(message) {
+    if (port == 3000) {
+        console.log(message);
+    }
+}
