4fbe4bc5d8f69e93941a7ce1e2923f38669a3470
Kyle Anderson <kylekandderson@gmail.com>
2019-10-19 10:29:54 -0400

    poc modal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,8 +10,6 @@ const resetContainer = document.getElementById("reset-container"); // reset butt
 const name = getUserName(); // get the users name
 const votingContainer = document.getElementById("voting-container"); // get the voting container element
 const messageContainer = document.getElementById("message-container"); // roster container
-const modal = document.getElementById("aboutModal");
-const aboutContainer = document.getElementById("about-container");
 
 var votingFinished = false; // used to track if each vote is the last vote or not
 
@@ -34,26 +32,6 @@ resetContainer.addEventListener("reset", e => {
     socket.emit("reset"); // emit the reset command to the socket
 });
 
-aboutContainer.addEventListener("button", e => {
-    e.preventDefault();
-    modal.style.display = "block";
-})
-
-var aboutBtn = document.getElementById("aboutButton");
-var span = document.getElementsByClassName("close")[0];
-aboutBtn.onclick = function () {
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-  }
-  
-  // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-    modal.style.display = "none";
-    }
-}
 
 /*
 ================================================================================================================
